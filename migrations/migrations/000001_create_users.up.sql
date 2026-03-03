b0673b654a24a51460c0fc5f92d5585f08e0b17c
Aneeshie <aneeshdas556@gmail.com>
2026-03-03 23:37:47 +0530

    feat: user migrations

---
+++ migrations/migrations/000001_create_users.up.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid();
+  email CITEXT UNIQUE NOT NULL,
+  password_hash TEXT NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+)
