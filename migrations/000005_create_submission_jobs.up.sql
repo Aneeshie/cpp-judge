dfce5e942b1d0fa424af9ebfe09e5dbc2c6112b8
Aneeshie <aneeshdas556@gmail.com>
2026-03-04 23:37:40 +0530

    feat: submission jobs table

---
+++ migrations/000005_create_submission_jobs.up.sql
@@ -0,0 +1,21 @@
+CREATE TYPE job_status AS ENUM (
+    'PENDING',
+    'PROCESSING',
+    'COMPLETED',
+    'FAILED'
+)
+
+CREATE TABLE submission_jobs (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+
+    submission_id UUID NOT NULL,
+    status job_status NOT NULL DEFAULT 'PENDING',
+
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+
+    CONSTRAINT fk_job_submission
+        FOREIGN KEY (submission_id)
+        REFERENCES submissions(id)
+        ON DELETE CASCADE
+)
